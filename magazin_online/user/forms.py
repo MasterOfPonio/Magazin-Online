7e7793ddd96b2c85eb4d154457f233631e9ce419
adriangabrieldoaga <adriangabrieldoaga@gmail.com>
2026-05-08 18:36:51 +0300

    Added new register functionality and fields

--- magazin_online/user/forms.py
+++ magazin_online/user/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import UserProfile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -9,4 +10,19 @@ class UserRegisterForm(UserCreationForm):
     last_name = forms.CharField(max_length = 20)
     class Meta:
         model = User
-        fields = ['username', 'email', 'phone_no', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'phone_no', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data.get('email')
+        user.first_name = self.cleaned_data.get('first_name')
+        user.last_name = self.cleaned_data.get('last_name')
+
+        if commit:
+            user.save()
+            # Create or update UserProfile with phone_no
+            UserProfile.objects.get_or_create(
+                user=user,
+                defaults={'phone_no': self.cleaned_data.get('phone_no')}
+            )
+        return user
